b454920f117846d57a5617c8dd2662ebfb33bcc8
Márk Kaposvári <xmarko45@gmail.com>
2021-03-12 21:00:40 +0100

    Task 2.5 and 2.6 done

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -1,10 +1,9 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
@@ -21,6 +20,27 @@ public class Main {
 		main(new String[0]);
 	}
 	
+	static Random random = new Random();
+	
+	public static String randomNameGenerator(Statechart s)
+	{
+		while(true)
+		{
+			String name = "NoNameState" + random.nextInt(Integer.MAX_VALUE);
+			TreeIterator<EObject> iterator = s.eAllContents();
+			while (iterator.hasNext()) {
+				EObject content = iterator.next();
+				if(content instanceof State) {
+					State state = (State) content;
+					if(state.getName() == name)
+						break;
+					
+				}
+				return name;
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		ModelManager manager = new ModelManager();
 		Model2GML model2gml = new Model2GML();
@@ -29,28 +49,43 @@ public class Main {
 		EObject root = manager.loadModel("model_input/example.sct");
 		
 		// Reading model
-		List<String> trappedStateNames = new ArrayList<String>();
+		List<State> trappedStates = new ArrayList<State>();
+		List<State> noNameStates = new ArrayList<State>();
 		Statechart s = (Statechart) root;
 		TreeIterator<EObject> iterator = s.eAllContents();
 		while (iterator.hasNext()) {
 			EObject content = iterator.next();
 			if(content instanceof State) {
 				State state = (State) content;
+				if(state.getName() == null)
+				{				
+					state.setName(randomNameGenerator(s));
+					noNameStates.add(state);
+				}
 				System.out.println(state.getName());
 				if(state.getOutgoingTransitions().isEmpty())
-					trappedStateNames.add(state.getName());
-					
+					trappedStates.add(state);					
 			}
 			else if(content instanceof Transition)
 			{
 				Transition tr = (Transition) content;
+				if(tr.getTarget().getName() == null)
+				{				
+					tr.getTarget().setName(randomNameGenerator(s));
+					noNameStates.add((State) tr.getTarget());
+				}
 				System.out.println(tr.getSource().getName() + " -> " + tr.getTarget().getName());
 			}
 		}
 		
+		System.out.print("Suggested names: ");
+		for(int i = 0; i < noNameStates.size(); i++)
+			System.out.print(noNameStates.get(i).getName() + " ");
+		System.out.println();
+		
 		System.out.print("Trapped state names: ");
-		for(int i = 0; i < trappedStateNames.size(); i++)
-			System.out.print(trappedStateNames.get(i) + " ");
+		for(int i = 0; i < trappedStates.size(); i++)
+			System.out.print(trappedStates.get(i).getName() + " ");
 		System.out.println();
 		
 		// Transforming the model into a graph representation
