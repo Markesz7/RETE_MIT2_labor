a858db4df6ac95eaa338d0617834193b7cf72376
Márk Kaposvári <xmarko45@gmail.com>
2021-03-13 18:50:04 +0100

    Task 4.4 and 4.5 done

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
 import org.yakindu.sct.model.sgraph.Transition;
+import org.yakindu.sct.model.stext.stext.EventDefinition;
+import org.yakindu.sct.model.stext.stext.VariableDefinition;
 
 import hu.bme.mit.model2gml.Model2GML;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
@@ -42,6 +44,8 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
+		
+		System.out.println("Task 2.3:\n");
 		ModelManager manager = new ModelManager();
 		Model2GML model2gml = new Model2GML();
 		
@@ -51,11 +55,23 @@ public class Main {
 		// Reading model
 		List<State> trappedStates = new ArrayList<State>();
 		List<State> noNameStates = new ArrayList<State>();
+		List<VariableDefinition> variables = new ArrayList<VariableDefinition>();
+		List<EventDefinition> events = new ArrayList<EventDefinition>();
 		Statechart s = (Statechart) root;
 		TreeIterator<EObject> iterator = s.eAllContents();
 		while (iterator.hasNext()) {
 			EObject content = iterator.next();
-			if(content instanceof State) {
+			if(content instanceof VariableDefinition)
+			{
+				VariableDefinition var = (VariableDefinition) content;
+				variables.add(var);
+			}
+			else if(content instanceof EventDefinition)
+			{
+				EventDefinition event = (EventDefinition) content;
+				events.add(event);
+			}
+			else if(content instanceof State) {
 				State state = (State) content;
 				if(state.getName() == null)
 				{				
@@ -78,19 +94,98 @@ public class Main {
 			}
 		}
 		
+		System.out.println("\nTask 2.5:\n");
 		System.out.print("Suggested names: ");
 		for(int i = 0; i < noNameStates.size(); i++)
 			System.out.print(noNameStates.get(i).getName() + " ");
 		System.out.println();
 		
+		System.out.println("\nTask 2.4:\n");
 		System.out.print("Trapped state names: ");
 		for(int i = 0; i < trappedStates.size(); i++)
 			System.out.print(trappedStates.get(i).getName() + " ");
 		System.out.println();
+		
+		System.out.println("\nTask 4.3:\n");
+		System.out.print("Variable names: ");
+		for(int i = 0; i < variables.size(); i++)
+			System.out.print(variables.get(i).getName() + " ");
+		System.out.println();
+		
+		System.out.print("Event names: ");
+		for(int i = 0; i < events.size(); i++)
+			System.out.print(events.get(i).getName() + " ");
+		System.out.println();		
+		System.out.println("\nTask 4.4 and 4.5:\n");
+		
+		System.out.println("package hu.bme.mit.yakindu.analysis.workhere;\r\n" + 
+				"\r\n" + 
+				"import java.io.BufferedReader;\r\n" + 
+				"import java.io.IOException;\r\n" + 
+				"import java.io.InputStreamReader;\r\n" + 
+				"\r\n" + 
+				"import hu.bme.mit.yakindu.analysis.RuntimeService;\r\n" + 
+				"import hu.bme.mit.yakindu.analysis.TimerService;\r\n" + 
+				"import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;\r\n" + 
+				"import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;\r\n" + 
+				"\r\n" + 
+				"\r\n" + 
+				"\r\n" + 
+				"public class RunStatechart {\r\n" + 
+				"	\r\n" + 
+				"	public static void main(String[] args) throws IOException {\r\n" + 
+				"		ExampleStatemachine s = new ExampleStatemachine();\r\n" + 
+				"		s.setTimer(new TimerService());\r\n" + 
+				"		RuntimeService.getInstance().registerStatemachine(s, 200);\r\n" + 
+				"		s.init();\r\n" + 
+				"		s.enter();\r\n" + 
+				"		s.runCycle();\r\n" + 
+				"		boolean exit = false;\r\n" + 
+				"		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\r\n" + 
+				"		while(!exit)\r\n" + 
+				"		{\r\n" + 
+				"			switch(br.readLine())\r\n" + 
+				"			{");
+		
+		for(int i = 0; i < events.size(); i++)
+		{
+			System.out.println("			case \"" + events.get(i).getName() + "\":\r\n" + 
+							   "				s.raise" + (events.get(i).getName().substring(0, 1).toUpperCase() + events.get(i).getName().substring(1)) +"();\r\n" + 
+							   "				break;\r\n");
+		}
+
+		System.out.println("			case \"exit\":\r\n" + 
+						   "				exit = true;\r\n" + 
+						   "				break;\r\n" + 
+						   "				\r\n" + 
+						   "			default:\r\n" + 
+						   "				System.out.println(\"Unknown command!\");\r\n" + 
+						   "				break;\r\n" + 
+						   "			}\r\n" + 
+						   "			if(!exit)\r\n" + 
+						   "			{\r\n" + 
+						   "				s.runCycle();\r\n" + 
+						   "				print(s);	\r\n" + 
+						   "			}\r\n" + 
+						   "		}\r\n" + 
+						   "		System.exit(0);\r\n" + 
+						   "	}\r\n");		
+		
+		System.out.println("\tpublic static void print(IExampleStatemachine s) {");
+		for(int i = 0; i < variables.size(); i++)
+		{
+			System.out.println("\t\tSystem.out.println(\"" 
+							  + variables.get(i).getName().substring(0, 1).toUpperCase() 
+							  + " = \" + s.getSCInterface().get"
+							  + (variables.get(i).getName().substring(0, 1).toUpperCase() + variables.get(i).getName().substring(1))
+							  +"());");
+		}
+		System.out.println("\t}\r\n}");
 		
 		// Transforming the model into a graph representation
 		String content = model2gml.transform(root);
 		// and saving it
 		manager.saveFile("model_output/graph.gml", content);
 	}
+	
 }
